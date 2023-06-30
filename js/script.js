8f6ae4437758f955ffd0468bf74943eb9dd49716
Serhii Bondariev <xx.code4u@gmail.com>
2023-06-30 23:37:49 +0300

    11111

--- js/script.js
+++ js/script.js
@@ -1,8 +1,135 @@
-let cost = 0;
-const subscription = "pro";
+// const x = 10;
+// const y = 5;
 
-if (subscription === "pro") {
-  cost = 100;
+// // Додавання
+// console.log(x + y); // 15
+
+// // Віднімання
+// console.log(x - y); // 5
+
+// // Множення
+// console.log(x * y); // 50
+
+// // Ділення
+// console.log(x / y); // 2
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+
+// // Change code below this line
+
+// const totalPrice = 3500 * 4;
+// console.log(3500 * 4);
+
+// const productName = "Droid";
+
+// const pricePerItem = 3500;
+
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`
+// console.log (message);
+
+
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = (pricePerDroid * orderedQuantity + deliveryFee);
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
+// console.log(message);
+
+// let cost;
+// const subscription = "premium";
+
+// if (subscription === "free") {
+//   cost = 0;
+// } else if (subscription === "pro") {
+//   cost = 100;
+// } else if (subscription === "premium") {
+//   cost = 500;
+// } else {
+//   console.log("Invalid subscription type");
+// }
+
+// console.log(cost); // 500
+
+// let cost;
+// const subscription = "premium";
+
+// switch (subscription) {
+//   case "free":
+//     cost = 0;
+//     break;
+
+//   case "pro":
+//     cost = 100;
+//     break;
+
+//   case "premium":
+//     cost = 500;
+//     break;
+
+//   default:
+//     console.log("Invalid subscription type");
+// }
+
+// console.log(cost); // 500
+
+
+
+// const global = "global";
+
+// if (true) {
+//   const blockA = "block A";
+
+//   // Бачимо глобальну + локальну A
+//   console.log(global); // 'global'
+//   console.log(blockA); // block A
+
+//   // Змінні blockB і blockC не знайдені в доступних областях видимості.
+//   // Буде помилка звернення до змінної.
+//   console.log(blockB); // ReferenceError: blockB is not defined
+//   console.log(blockC); // ReferenceError: blockC is not defined
+
+//   if (true) {
+//     const blockB = "block B";
+
+//     // Бачимо глобальну + зовнішню A + локальну B
+//     console.log(global); // global
+//     console.log(blockA); // block A
+//     console.log(blockB); // block B
+
+//     // Змінна blockC не знайдена в доступних областях видимості.
+//     // Буде помилка звернення до змінної.
+//     console.log(blockC); // ReferenceError: blockC is not defined
+//   }
+// }
+
+// if (true) {
+//   const blockC = "block C";
+
+//   // Бачимо глобальну + локальну C
+//   console.log(global); // global
+//   console.log(blockC); // block C
+
+//   // Змінні blockA і blockB не знайдені в доступних областях видимості.
+//   // Буде помилка звернення до змінної.
+//   console.log(blockA); // ReferenceError: blockA is not defined
+//   console.log(blockB); // ReferenceError: blockB is not defined
+// }
+
+// // Бачимо лише глобальну
+// console.log(global); // global
+
+// // Змінні blockA, blockB і blockC не знайдені в доступних областях видимості.
+// // Буде помилка звернення до змінної.
+// console.log(blockA); // ReferenceError: blockA is not defined
+// console.log(blockB); // ReferenceError: blockB is not defined
+// console.log(blockC); // ReferenceError: blockC is not defined
+
+
+const value = 10;
+
+if (value === 10) {
+  console.log("condition is true")
+} else {
+console.log("condition is false")
 }
-
-console.log(cost); // 100
