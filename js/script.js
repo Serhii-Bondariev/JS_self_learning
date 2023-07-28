57665e4bb2a38474e0bae02c14cfa96b5c772cfc
Serhii Bondariev <xx.code4u@gmail.com>
2023-07-28 22:25:58 +0300

    sccs

--- js/script.js
+++ js/script.js
@@ -662,14 +662,14 @@
 // console.log(normalizeInput("This ISN'T SpaM"));
 // console.log(normalizeInput("Big SALE"));
 
-function checkForName(fullName, name) {
-  const result = fullName.includes(name); // Change this line
-  return result;
-}
+// function checkForName(fullName, name) {
+//   const result = fullName.includes(name); // Change this line
+//   return result;
+// }
 
-console.log(checkForName("Egor Kolbasov", "Egor"));
-console.log(checkForName("Egor Kolbasov", "egor"));
-console.log(checkForName("Egor Kolbasov", "egOr"));
-console.log(checkForName("Egor Kolbasov", "Zhenya"));
-console.log(checkForName("Vadim Nekrasov", "vadim"));
-console.log(checkForName("Vadim Nekrasov", "Dima"));
+// console.log(checkForName("Egor Kolbasov", "Egor"));
+// console.log(checkForName("Egor Kolbasov", "egor"));
+// console.log(checkForName("Egor Kolbasov", "egOr"));
+// console.log(checkForName("Egor Kolbasov", "Zhenya"));
+// console.log(checkForName("Vadim Nekrasov", "vadim"));
+// console.log(checkForName("Vadim Nekrasov", "Dima"));
