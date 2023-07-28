79c104051ac3a2339788cfe33948f7880a782317
Serhii Bondariev <xx.code4u@gmail.com>
2023-07-28 22:25:52 +0300

    Update new.js

--- js/new.js
+++ js/new.js
@@ -125,17 +125,16 @@
 //   });
 // }
 
-
 function checkStorage(available, ordered) {
-    let message;
-    // Change code below this line
-  
-    if (ordered > available) {
-      message = "Not enough goods in stock!";
-    } else {
-      message = "The order is accepted, our manager will contact you";
-    }
-  
-    // Change code above this line
-    return message;
+  let message;
+  // Change code below this line
+
+  if (ordered > available) {
+    message = "Not enough goods in stock!";
+  } else {
+    message = "The order is accepted, our manager will contact you";
   }
+
+  // Change code above this line
+  return message;
+}
