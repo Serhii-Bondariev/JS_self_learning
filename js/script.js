4ae66bdd518fbb3bef150a07a4c9287161d0ca5c
Serhii Bondariev <xx.code4u@gmail.com>
2023-07-29 11:38:50 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -673,3 +673,16 @@
 // console.log(checkForName("Egor Kolbasov", "Zhenya"));
 // console.log(checkForName("Vadim Nekrasov", "vadim"));
 // console.log(checkForName("Vadim Nekrasov", "Dima"));
+
+// function checkForSpam(message) {
+//   const lowerCaseMessage = message.toLowerCase();
+//   return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
+// }
+
+// console.log(checkForSpam("Latest technology news"));
+// console.log(checkForSpam("JavaScript weekly newsletter"));
+// console.log(checkForSpam("Get best sale offers now!"));
+// console.log(checkForSpam("Amazing SalE, only tonight!"));
+// console.log(checkForSpam("Trust me, this is not a spam message"));
+// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+// console.log(checkForSpam("[SPAM] How to earn fast money?"));
